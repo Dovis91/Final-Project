f47ff4cb21be9ad6f6b5d0d511ce853397ff6e06
Dovis91 <dovydas.ziukas@gmail.com>
2022-07-13 16:19:24 +0300

    app.jsx  with extra components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,34 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Register from "./Components/LoginRegister/Register";
 import Login from "./Components/LoginRegister/Login";
+import Home from "./Components/Home/Home";
+import Create from "./Components/Create/Create";
 
 function App() {
+  const navigate = useNavigate();
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setUserLoggedIn(true);
+      navigate("/");
+    }
+  }, []);
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn} />
       <Routes>
+        <Route path="/" element={<Home userLoggedIn={userLoggedIn} />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={<Login setUserLoggedIn={setUserLoggedIn} />}
+        />
+        <Route path="/create" element={<Create />} />
       </Routes>
     </div>
   );
